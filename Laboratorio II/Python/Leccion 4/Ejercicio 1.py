8877594ae9da1aa282f6f484e5e28b68495c3cf1
unknown <urielpardod712@gmail.com>
2023-08-28 22:07:24 -0300

    Clase 28/08 Diccionarios en python, HolaMundo JS

---
+++ Laboratorio II/Python/Leccion 4/Ejercicio 1.py
@@ -0,0 +1,3 @@
+numeros = list(range(0, 10, 3))
+for numero in numeros:
+    print(numero)
---
+++ Laboratorio II/Python/Leccion 4/Ejercicio tupla.py
@@ -0,0 +1,8 @@
+tupla = (13, 1, 8, 3, 2, 5, 8)
+numeros = []
+for tup in tupla:
+    if tup<5:
+        numeros.append(tup)
+print(numeros)
+for numero in numeros:
+    print(numero)

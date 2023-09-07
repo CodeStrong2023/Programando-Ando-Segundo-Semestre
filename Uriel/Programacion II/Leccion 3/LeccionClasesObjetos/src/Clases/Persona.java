cef4ec5484f5c22275afc818dcdfab7979036cd3
unknown <urielpardod712@gmail.com>
2023-09-07 17:24:57 -0300

    Commit clase 3

--- Uriel/Programacion II/Leccion 3/LeccionClasesObjetos/src/Clases/Persona.java
+++ Uriel/Programacion II/Leccion 3/LeccionClasesObjetos/src/Clases/Persona.java
@@ -1,5 +1,11 @@
 package Clases;
 
 public class Persona {
+    String nombre;
+    String apellido;
     
+    public void obtenerInformacion(){
+        System.out.println("Nombre "+nombre);
+        System.out.println("Apellido " +apellido );
+    }
 }

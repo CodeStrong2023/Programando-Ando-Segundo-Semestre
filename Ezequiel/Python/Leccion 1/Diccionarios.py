90b2e2f9b6df6462999e2b13dba09b53fce9ccf1
Ezequiel Lorenz <eze_lorenz@hotmail.com>
2023-09-07 08:38:49 -0300

    Finalizando con los diccionarios

--- Ezequiel/Python/Leccion 1/Diccionarios.py
+++ Ezequiel/Python/Leccion 1/Diccionarios.py
@@ -21,3 +21,33 @@ print(diccionario.get('POO'))
 #modificaion de los elementos del diccionario
 diccionario['IDE'] = ' Entorno de Desarrollo Integrado'
 print(diccionario)
+
+#recorrer elementos del diccionario
+#solo mmostrando la llave
+for termino in diccionario:
+    print(termino)
+
+#para recorrer la llave y el valor, debemos usar una funcion item()
+for termino, valor in diccionario.items():
+    print(termino,valor)
+
+#accediendo usando la funcion keys() que nos da las llaves
+for termino in diccionario.keys():
+    print(termino)
+
+#accediendo usando la funcion values() que nos da los valores
+for valor in diccionario.values():
+    print(valor)
+
+#comprobar la existencia de algun elemtneo devuelve un bool
+print('IDE' in diccionario)
+
+#agregar un elemtno
+diccionario['PK'] = 'Primary key'
+
+#eliminar un elemento
+diccionario.pop('POO')
+print(diccionario)
+
+#vaciar un diccionario
+diccionario.clear()

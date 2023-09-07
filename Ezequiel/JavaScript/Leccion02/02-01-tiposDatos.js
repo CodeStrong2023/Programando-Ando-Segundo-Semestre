90f918e617dba23187b4b2b6bf83055a01eb90c7
Ezequiel Lorenz <eze_lorenz@hotmail.com>
2023-09-07 10:37:36 -0300

    creacion de mas tipos de datos, num, str, obj

--- Ezequiel/JavaScript/Leccion02/02-01-tiposDatos.js
+++ Ezequiel/JavaScript/Leccion02/02-01-tiposDatos.js
@@ -2,3 +2,17 @@
 /*Comentario
 de
 muchas lineas */
+
+var nombre = 'ezequiel';//tipo string
+console.log(nombre);
+
+var numero = 10923431; //tipo numerico
+console.log(numero);
+
+var objeto = {
+    nombre: "Ezequiel",
+    apellido: "Lorenz",
+    telefono: "12312414"
+}
+
+console.log(objeto)

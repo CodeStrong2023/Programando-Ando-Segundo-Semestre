3d61117a66926a16157dd8d4e6ab1af854fb5f14
Ezequiel Lorenz <eze_lorenz@hotmail.com>
2023-09-07 10:09:09 -0300

    Tematica Pilas fifo

---
+++ Ezequiel/Python/Leccion 1/Pilas.py
@@ -0,0 +1,31 @@
+#Se crea una pila usando listas
+
+pila = [1,2,3]
+#se agrega elementos a la pila por el fiman
+
+pila.append(4)
+
+pila.append(5)
+print(pila)
+
+#Se elimina los elementoo desde el final
+#se lo elimina y se lo guarda en una variable
+elementoBorrado = pila.pop()
+
+print(f'Sacamos el elemento {elementoBorrado}')
+print(f'Asi nos quedo lo pila: {pila}')
+
+
+#COLAS: datos de tipo fifo (first input / first output)
+
+cola = ['Ariel','Osvaldo','Liliana','Pilar',]
+
+#agregamos elementos al final de la cola
+cola.append('Natalia')
+cola.append('Jose')
+print(cola)
+
+#Sacamos elementos de la cola
+seRetira = cola.pop(0)
+print(f'Atendido el cliente: {seRetira}')
+print(cola)

f09658b8600659441df75a059304112bfe402038
Ezequiel Lorenz <eze_lorenz@hotmail.com>
2023-09-08 09:29:38 -0300

    Clase 1 y 2 con tarea incluida

--- Ezequiel/Java/Leccion 2/CicloWhile/src/CicloWhile/EjercicioWhile01.java
+++ Ezequiel/Java/Leccion 2/CicloWhile/src/CicloWhile/EjercicioWhile01.java
@@ -15,5 +15,18 @@ public class EjercicioWhile01 {
             System.out.println("contador = " + contador);
             contador++;
         }while(contador < 7);
+        
+        for(var contando = 0; contando < 7; contando++){
+            if(contando % 2 == 0){
+                System.out.println("contando = " + contando);
+                break;
+            }
+        }
+        for(var contando = 0; contando < 7; contando++){
+            if(contando % 2 != 0){
+                continue;
+            }
+            System.out.println("contando = " + contando);
+        }
     }
 }

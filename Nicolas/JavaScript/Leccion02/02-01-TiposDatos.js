92aaaa1a032f7619bfedc38ab6f8cebf4547a1cf
Nicolas Sini <nicobloord1@gmail.com>
2023-09-12 11:55:30 -0300

    Tipos de Datos Clase 3 JavaScript

---
+++ Nicolas/JavaScript/Leccion02/02-01-TiposDatos.js
@@ -0,0 +1,14 @@
+var nombre = "Ariel"; //Tipo String
+console.log(nombre);
+
+var numero = 3000; //Tipo Numerico
+console.log(numero);
+
+var objeto = {
+    nombre : "Ariel",
+    apellido : "Betancud",
+    telefono : "2604088892"
+
+}
+
+console.log(objeto);

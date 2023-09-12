6a5b2bfe6eeda7e423072246001ebb50be1f7dae
Nicolas Sini <nicobloord1@gmail.com>
2023-09-12 12:54:03 -0300

    Clase 4 JavaScript, Tipos de Datos (boolean,funcion,symbol) y Clases

--- Nicolas/JavaScript/Leccion02/02-01-TiposDatos.js
+++ Nicolas/JavaScript/Leccion02/02-01-TiposDatos.js
@@ -1,5 +1,8 @@
 var nombre = "Ariel"; //Tipo String
 console.log(nombre);
+var nombre = 7;
+console.log(typeof nombre);
+
 
 var numero = 3000; //Tipo Numerico
 console.log(numero);
@@ -12,3 +15,25 @@ var objeto = {
 }
 
 console.log(objeto);
+
+
+// Tipo de Dato Boolean
+var bandera = true;
+console.log(bandera);
+
+// Tipo de Dato Funcion
+function miFuncion (){}
+console.log(typeof miFuncion);
+
+// Tipo de Dato Symbol
+var simbolo = Symbol("Mi Simbolo");
+console.log(typeof simbolo);
+
+// Tipo de Dato Clase
+class Persona{
+    constructor(nombre,apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
+console.log(typeof Persona);

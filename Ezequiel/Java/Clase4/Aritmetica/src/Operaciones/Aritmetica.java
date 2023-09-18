5c91927924205806594efa7dc866b9fa6b05dcad
Ezequiel Lorenz <eze_lorenz@hotmail.com>
2023-09-18 08:02:55 -0300

    Clase 4, Programacion, creacion de metodos, recorrer paso a paso, pasar argumentos, operador this

--- Ezequiel/Java/Clase4/Aritmetica/src/Operaciones/Aritmetica.java
+++ Ezequiel/Java/Clase4/Aritmetica/src/Operaciones/Aritmetica.java
@@ -9,4 +9,22 @@ public class Aritmetica {
         int resultado = a + b;
         System.out.println("resultado = " + resultado);
     }
+    
+    public int sumarConRetorno(){
+        return a + b;
+    }
+    
+    public int sumarConArgumentos(int arg1, int arg2){
+        a = arg1;
+        b = arg2;
+        return sumarConRetorno();
+    }
+    
+    public int sumarConArgumentosThis(int a, int b){
+        this.a = a;
+        this.b = b;
+        return sumarConRetorno();
+    }
+    
+    
 }

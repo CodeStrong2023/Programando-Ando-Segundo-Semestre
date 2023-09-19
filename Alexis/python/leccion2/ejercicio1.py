7bdcef64921a3f8de710bb3f990675d20878c9cc
Alitoo27 <perezalexismartin575@gmail.com>
2023-09-19 20:22:31 -0300

    subiendo archivos de python

---
+++ Alexis/python/leccion2/ejercicio1.py
@@ -0,0 +1,10 @@
+"""
+ejercicio 1
+debemos plasmar la expresion en una expresion
+algoritmica, osea hacerlo en codigo
+"""
+a = float(input("digite el valor de a: "))
+b = float(input("digite el valor de b: "))
+c = float(input("digite el valor de c: "))
+resultado = (a ** 3 * (b ** 2 - 2 * a * c))/(2 * b)
+print(f"el resultado es: {resultado}")
---
+++ Alexis/python/leccion2/ejercicio5.py
@@ -0,0 +1,19 @@
+#ejercicio: sistema de la calificaciones
+calificacion = int(input("digite una calificacion entre 0 y 10: "))
+if 9 <= calificacion <=10:
+    print("A")
+elif 8 <= calificacion <9:
+    print("B")
+elif 7 <= calificacion <8:
+    print("C")
+elif 6 <= calificacion <7:
+    print("D")
+elif 0 <= calificacion <6:
+    print("F")
+else:
+    print("valor incorrecto...")
+
+
+
+
+

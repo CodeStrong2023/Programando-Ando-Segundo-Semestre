6eb815c7f49c788c4e61553d34134a671c083847
Ezequiel Lorenz <eze_lorenz@hotmail.com>
2023-09-19 07:42:12 -0300

    Laboratorio 2, Clase 5 Python, Ejercicios 4 y 5,Sumar pares en un rango y calcular factorial

---
+++ Ezequiel/Python/Leccion 1/Ejercicio05.py
@@ -0,0 +1,13 @@
+# Ejercicio 5: factorial de un numero positiv0
+# acer un programa para calcular el factorial de n numero positivo
+
+numero = int(input('Digite un numero: '))
+while numero < 0:
+    print("Error => El numero tiene que ser positivo")
+    numero = int(input("Digite un numero: "))
+
+factorial = 0
+for i in range(1,numero+1)
+    factorial *= i
+
+print(f"\nEl factorial del numero {numero} positivo ingresado es: {factorial}")

29dbd382c5813ff40f42ff159cb8fc2f64e844c6
Silvio Stefanucci <silvioshredderstefanucci@gmail.com>
2023-09-20 11:45:59 -0300

    Adding Python excercises

---
+++ Silvio/Laboratorio II/Phyton/Leccion4/EjercicioColecciones1.py
@@ -0,0 +1,6 @@
+# Eliminar elementos repetidos de una lista
+ingredientes = ['limon', 'ajo', 'pimienta', 'morron', 'vinagre', 'aceite', 'limon']
+aux = set(ingredientes)
+ing_sin_repetir = list(aux)
+
+print(ing_sin_repetir)

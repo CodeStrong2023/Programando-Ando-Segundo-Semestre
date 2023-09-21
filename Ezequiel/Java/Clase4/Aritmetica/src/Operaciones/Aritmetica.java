c108e006bf3d80e022ca553090dd748fcf4a70d0
Ezequiel Lorenz <eze_lorenz@hotmail.com>
2023-09-21 07:54:49 -0300

    Programacion 2, Java, Clase 6, Sobrecarga de ctor, alcance de variables, memoria stack y heap

--- Ezequiel/Java/Clase4/Aritmetica/src/Operaciones/Aritmetica.java
+++ Ezequiel/Java/Clase4/Aritmetica/src/Operaciones/Aritmetica.java
@@ -5,6 +5,18 @@ public class Aritmetica {
     int a;
     int b;
     
+    //Sobrecarga de contructores
+    public Aritmetica(){
+        System.out.println("Se esta ejecutando el constructor numero uno");
+    }
+    
+    public Aritmetica(int a, int b){
+        this.a = a;
+        this.b = b;
+        System.out.println("Se esta Ejecutando el constructor numero dos");
+    }
+    
+    
     public void sumarNumeros(){
         int resultado = a + b;
         System.out.println("resultado = " + resultado);

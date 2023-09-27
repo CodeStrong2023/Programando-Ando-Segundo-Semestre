24a0aaae577aaec6864bf58da240c850f4b4b770
Ezequiel Lorenz <eze_lorenz@hotmail.com>
2023-09-27 14:17:28 -0300

    Laboratorio 2, Python, Clase 6, Funciones parte 2, List Unpacking, repaso ciclo for else, List Comprehension, paso de argumentos, palabra return

--- Ezequiel/Python/Leccion5/main.py
+++ Ezequiel/Python/Leccion5/main.py
@@ -6,3 +6,58 @@ def mi_funcion():
 
 mi_funcion()
 mi_funcion()
+
+# Desempaquetado de lista o List Unpacking
+def show(name, lastName):
+    print(name+ lastName)
+
+person = ["Ezequiel", "Lorenz"]
+show(person[0], person[1]) #Pasamos uno por uno los datos de la lista a la funcion
+show(*person) # es lo mismo que lo anterior pero le pasamos todo junto
+
+person2 = ["Ariel", "Betancud"]
+show(*person2)
+person3 = {"lastName" : "Lucero", "name" : "Natalia"}
+show(**person3)
+
+## Repaso de ciclo for
+
+numbers = [1,2,3,4,5]
+for n in numbers:
+    print(n)
+    if n == 3:
+        break ## esto hace que no se ejecute el siguiente else
+else:
+    print('Esto se termino')
+
+## List Comprehension, lista de compresion
+
+names = ["Paolo", "Rodrigo", "Lupe", "Pepe"]
+alongP = [p for p in names if p[0] == 'P']
+print(alongP)
+
+bottleC = [ {'name': "Quilmes", "country": "Arg"},
+            {'name': "Corona", "country": "Mx"},
+            {'name': "Stella Artois", "country": "Belgium"}
+           ]
+Arg = [b for b in bottleC if b["country"] == "Arg"]
+print(Arg)
+print(bottleC)
+
+## Paso de argumentos
+
+def mi_funcion2(name, lastName):
+    print('Saludo a todos los profes de la tecnicatura')
+    print(f'Nombre: {name}, Apellido: {lastName}')
+mi_funcion2('Ariel', 'Betancud')
+mi_funcion2('Jorge', 'Lucero')
+mi_funcion2('Natalia', 'Lucero')
+
+## palabra return en funciones
+
+def sumar(a,b):
+    return a + b
+
+resultado = sumar(78 + 22)
+# print(f'El resultado  de la suma es: {resultado}')
+print(f'El resultado  de la suma es: {sumar(78 + 22)}')

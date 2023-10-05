c67da8dbb41c9e106fa6d382de4c8485cd3ad857
Alitoo27 <perezalexismartin575@gmail.com>
2023-10-05 13:35:37 -0300

    javascript clase 3

---
+++ Alexis/javascript/leccion02/02-01-TiposDatos.js
@@ -0,0 +1,23 @@
+// tipos de datos en javascscript
+
+/*
+la sintaxis en lo que es comentarios
+es muy similar a la de java
+realmente diriamos que es identica
+*/
+
+var nombre = "ariel"; // tipo str
+console.log(nombre);
+
+var numero = 3000; // tipo numerico
+console.log(numero);
+
+var objeto = {
+    nombre : "ariel",
+    apellido : "betancut",
+    telefono : "12345678"
+}
+console.log(objeto);
+
+
+

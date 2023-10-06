7dfd4c57a7cbf2c3e15bf1952513498608668110
Alitoo27 <perezalexismartin575@gmail.com>
2023-10-05 21:08:23 -0300

    javascript clase 4

--- Alexis/javascript/leccion02/02-01-TiposDatos.js
+++ Alexis/javascript/leccion02/02-01-TiposDatos.js
@@ -7,7 +7,11 @@ realmente diriamos que es identica
 */
 
 var nombre = "ariel"; // tipo str
-console.log(nombre);
+console.log(typeof nombre);
+nombre = 7;
+console.log(typeof nombre);
+nombre = 12.3;
+console.log(typeof nombre);
 
 var numero = 3000; // tipo numerico
 console.log(numero);
@@ -19,5 +23,26 @@ var objeto = {
 }
 console.log(objeto);
 
+// tipos de datos boolean
+var bandera = true;
+console.log(bandera);
+
+//tipo de dato funcion
+function miFuncion(){}
+console.log(miFuncion);
+
+//tipo de dato symbol
+var simbolo = Symbol("mi simbolo");
+console.log(simbolo);
+
+//tipo de dato clase
+class persona{
+    constructor(nombre, apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
+
+console.log(persona);
 
 

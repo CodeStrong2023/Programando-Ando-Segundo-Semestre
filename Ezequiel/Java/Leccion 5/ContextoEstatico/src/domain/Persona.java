5efba565d2736d8969433db49a50ffa3e4fc1330
Ezequiel Lorenz <eze_lorenz@hotmail.com>
2023-10-08 15:05:32 -0300

    Programacion 2, Java, clase 8, Encapsulamiento y contexto estatico

---
+++ Ezequiel/Java/Leccion 5/ContextoEstatico/src/domain/Persona.java
@@ -0,0 +1,41 @@
+
+package domain;
+
+/*
+ */
+public class Persona {
+    private int idPersona;
+    private static int contadorPersona;
+    private String nombre;
+    
+    //Constructor
+    public Persona(String nombre){
+        this.nombre = nombre;
+        //Incrementar el contador por cada objeto nuevo
+        Persona.contadorPersona ++;
+        //Se asigna un nuevo valor a la variable idPersona
+        this.idPersona = Persona.contadorPersona;
+    }
+    
+    public int getIdPersona(){
+        return idPersona;
+    }
+    
+    public void setIdPersona(int idPersona){
+        this.idPersona = idPersona;
+    }
+    
+    public String getNombre(){
+        return this.nombre;
+    }
+    
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "idPersona=" + idPersona + ", nombre=" + nombre + '}';
+    }
+    
+}

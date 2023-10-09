cdc3e3d4b332e3572d6d57b05613899ac248a7ab
Nicolas Sini <nicobloord1@gmail.com>
2023-10-08 22:00:37 -0300

    Clase 5: Creamos Leccion05 y agregamos ejercicios nuevos

---
+++ Nicolas/Python/Leccion5/main.py
@@ -0,0 +1,8 @@
+# Comenzamos con Funciones
+# mi_funcion() # No se puede llamar antes de definir una funcion
+# Definimos una funcion
+def mi_funcion():
+    print("Saludos a todos los alumnos de la Tecnicatura")
+
+mi_funcion() # Estamos llamando a la funcion
+mi_funcion() # Se puede llamar a una funcion N cantidad de veces

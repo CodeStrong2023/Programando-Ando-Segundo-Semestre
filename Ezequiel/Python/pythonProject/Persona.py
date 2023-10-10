de58c0f85a3212dcb6adbb01334a7eb11fc94c40
Ezequiel Lorenz <eze_lorenz@hotmail.com>
2023-10-10 07:24:46 -0300

    Laboratorio 2, Python, Clase 8, Creacion de objetos con argumentos

--- Ezequiel/Python/pythonProject/Persona.py
+++ Ezequiel/Python/pythonProject/Persona.py
@@ -1,10 +1,10 @@
 class Persona # Creamosuna clase
-    def __init__(self): #se lo llama metodo Init Dunder
-        self.nombre = 'Juan'
-        self.apellido = 'Zalazar'
-        self.edad = 22
+    def __init__(self,nombre, apellido, edad): #se lo llama metodo Init Dunder
+        self.nombre = nombre
+        self.apellido = apellido
+        self.edad = edad
 
-persona1 = Persona()
+persona1 = Persona('Ezequiel', 'Lorenz', 28) #Necesitamos enviar argumentos
 print(persona1.nombre)
 print(persona1.apellido)
 print(persona1.edad)

575c4a67eef2f876f8372f887fe730e5b1f10f5c
Ezequiel Lorenz <eze_lorenz@hotmail.com>
2023-10-11 08:57:56 -0300

    Laboratoria 2, Clase 8, Python, Creacion y modificacion de un objeto

--- Ezequiel/Python/pythonProject/Persona.py
+++ Ezequiel/Python/pythonProject/Persona.py
@@ -1,4 +1,4 @@
-class Persona # Creamosuna clase
+class Persona: # Creamosuna clase
     def __init__(self,nombre, apellido, edad): #se lo llama metodo Init Dunder
         self.nombre = nombre
         self.apellido = apellido
@@ -10,3 +10,11 @@ print(persona1.apellido)
 print(persona1.edad)
 
 print(type(Persona))
+
+persona2 = Persona('Osvaldo', 'Gioradani', 45)
+print(f'El objeto2 de la clase persona: {persona2.nombre} {persona2.apellido} Su edad es: {persona2.edad}')
+
+persona1.nombre = 'Liliana'
+persona1.apellido = 'Buccella'
+persona1.edad = 40
+print(f'El objeto1 modificado de la clase persona: {persona1.nombre} {persona1.apellido} Su edad es: {persona1.edad}')

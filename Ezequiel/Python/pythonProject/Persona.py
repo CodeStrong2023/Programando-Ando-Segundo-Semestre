9c1e698df7422362798dd507f3f8054bb9207d99
Ezequiel Lorenz <eze_lorenz@hotmail.com>
2023-10-11 09:04:11 -0300

    Laboratorio 2, Python, Clase 8, Metodos de instancia, definicion de un metodo

--- Ezequiel/Python/pythonProject/Persona.py
+++ Ezequiel/Python/pythonProject/Persona.py
@@ -4,6 +4,9 @@ class Persona: # Creamosuna clase
         self.apellido = apellido
         self.edad = edad
 
+    def mostrar_detalle(self):
+        print(f'Persona: {self.nombre} {self.apellido} {self.edad}')
+
 persona1 = Persona('Ezequiel', 'Lorenz', 28) #Necesitamos enviar argumentos
 print(persona1.nombre)
 print(persona1.apellido)
@@ -18,3 +21,8 @@ persona1.nombre = 'Liliana'
 persona1.apellido = 'Buccella'
 persona1.edad = 40
 print(f'El objeto1 modificado de la clase persona: {persona1.nombre} {persona1.apellido} Su edad es: {persona1.edad}')
+
+#Los atributos son caracteristicas
+# Los metodos son el comportamiento que van a tener los objetos (acciones)
+persona1.mostrar_detalle()
+persona2.mostrar_detalle()

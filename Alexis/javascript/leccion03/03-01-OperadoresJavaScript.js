9ececdb85d99a2108d70686d95d746ed90f9a822
Alitoo27 <perezalexismartin575@gmail.com>
2023-10-11 12:24:12 -0300

    javascript clase 8

--- Alexis/javascript/leccion03/03-01-OperadoresJavaScript.js
+++ Alexis/javascript/leccion03/03-01-OperadoresJavaScript.js
@@ -15,3 +15,15 @@ if (edad >= adulto){
 else{
     console.log("usted es una persona menor de edad")
 }
+
+//ejercicio: dentro de un rango
+let dentroRango = 5; //aqui vamos a ir cambiando el valor
+let valMin = 0, valMax = 10;
+if(dentroRango >= valMin && dentroRango <= valMax){
+    console.log("esta dentro del rango establecido")
+}
+else{
+    console.log("esta fuera del rango establecido")
+}
+
+

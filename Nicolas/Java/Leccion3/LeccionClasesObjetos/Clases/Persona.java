4f05b4d654b7dfb18311d857902c266c29be9638
Nicolas Sini <nicobloord1@gmail.com>
2023-10-11 13:37:24 -0300

    Clase 4 Java: Creacion Leccion04, Clases y Objetivos Parte 2

---
+++ Nicolas/Java/Leccion3/LeccionClasesObjetos/Clases/Persona.java
@@ -0,0 +1,11 @@
+public class Persona { 
+    // Atributos de la clase (Caracteristicas)
+    String nombre;
+    String apellido;
+
+    //Metodo de la clase (Acciones)
+    public void obtenerInformacion() {
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Apellido: "+apellido);
+    }
+}
---
+++ Nicolas/Java/Leccion4/Operaciones/Aritmetica.java
@@ -0,0 +1,15 @@
+public class Aritmetica {
+    
+        // Atributos de la clase
+        int a;
+        int b;
+
+        // Metodo
+        public void sumarNumeros() {
+            int resultado = a + b;
+            System.out.println("El resultado es: "+resultado);
+        }
+
+        
+    
+}

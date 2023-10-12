c6a54aebf2354dcc285ff82d2f7243e2637c80ca
unknown <urielpardod712@gmail.com>
2023-10-12 19:58:39 -0300

    JS Leccion01 y Leccion02

---
+++ Uriel/Laboratorio II/JS/Leccion02/02-01-TiposDeDatos.js
@@ -0,0 +1,17 @@
+//Tipos de datos en JS
+/*
+Tipos de comentarios lineal o por parrafo
+*/
+var nombre = 'Uriel'; //Tipo str
+console.log(nombre);
+
+var numero = 3000; //Tipo numerico
+console.log(numero);
+
+var objeto = { //Tipo Objeto
+    nombre : "Uriel",
+    apellido : "Pardo",
+    edad : 27,
+    telefono : "2604121212"
+}
+console.log(objeto);

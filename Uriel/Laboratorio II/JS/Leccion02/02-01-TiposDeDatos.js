d4f57ff561ea55f594890823c738a0d2656bda34
unknown <urielpardod712@gmail.com>
2023-10-12 20:31:57 -0300

    Calse 3 y 4

--- Uriel/Laboratorio II/JS/Leccion02/02-01-TiposDeDatos.js
+++ Uriel/Laboratorio II/JS/Leccion02/02-01-TiposDeDatos.js
@@ -5,6 +5,14 @@ Tipos de comentarios lineal o por parrafo
 var nombre = 'Uriel'; //Tipo str
 console.log(nombre);
 
+nombre = 7;
+console.log(nombre);
+console.log(typeof nombre);
+
+nombre = 12.3;
+console.log(nombre);
+console.log(typeof nombre);
+
 var numero = 3000; //Tipo numerico
 console.log(numero);
 
@@ -15,3 +23,24 @@ var objeto = { //Tipo Objeto
     telefono : "2604121212"
 }
 console.log(objeto);
+
+//Tipo de dato boolean
+var bandera = true;
+console.log(bandera);
+
+//FUnciones son tipos de datos y se pueden reutilizar llamando en repetidas ocasiones
+function miFuncion(){}
+console.log(typeof miFuncion);
+
+//Tipo de dato symbol
+var simbolo = Symbol("Mi simbolo");
+console.log(typeof simbolo)
+
+//Tipo de dato Clase
+class Persona{
+    constructor(nombre, apellido){
+        this.nombre;
+        this.apellido;
+    }
+}
+console.log(Persona);

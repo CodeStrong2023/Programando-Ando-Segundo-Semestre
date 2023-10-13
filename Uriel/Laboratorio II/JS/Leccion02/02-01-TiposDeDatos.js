e364f48b4027c74d06e65eb048d00d166fbd6af9
unknown <urielpardod712@gmail.com>
2023-10-12 21:12:49 -0300

    Javascript Clase 5

--- Uriel/Laboratorio II/JS/Leccion02/02-01-TiposDeDatos.js
+++ Uriel/Laboratorio II/JS/Leccion02/02-01-TiposDeDatos.js
@@ -44,3 +44,23 @@ class Persona{
     }
 }
 console.log(Persona);
+
+//Tipo de dato undefined
+var x;
+console.log(typeof x);
+x = undefined;
+console.log(x);
+
+//nul : significa ausencia de valor, esta vacio
+var y = null; //null no es un tipo de dato pero su origen es object
+console.log(typeof y);
+
+//Tipo de dato array y Empty String
+var autos = ['Citroen', 'Audi', 'BMW', 'Ford'];
+console.log(autos);
+console.log(typeof autos);
+
+//Datos vacios Empty
+var z = ''; //Es una cadena vacia
+console.log(z);
+console.log(typeof z);

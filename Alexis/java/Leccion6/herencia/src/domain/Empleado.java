ff073670e57c6ae76bec9e3b731e985aaea42abb
Alitoo27 <perezalexismartin575@gmail.com>
2023-10-17 21:24:45 -0300

    java clase 9

---
+++ Alexis/java/Leccion6/herencia/src/domain/Empleado.java
@@ -0,0 +1,46 @@
+
+package domain;
+
+public class Empleado extends Persona{
+    private int idEmpleado;
+    private double sueldo;
+    private static int contadorEmpleados; //es para incrementar
+    
+    //constructor
+    public Empleado(String nombre, double sueldo) {
+        super(nombre);
+        this.idEmpleado = ++Empleado.contadorEmpleados;
+        this.sueldo = sueldo;
+    }
+
+    public int getIdEmpleado() {
+        return this.idEmpleado;
+    }
+
+    public void setIdEmpleado(int idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public double getSueldo() {
+        return this.sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Empleado{idEmpleado=").append(idEmpleado);
+        sb.append(", sueldo=").append(sueldo);
+        sb.append('}');
+        return sb.toString();
+    }
+    
+    
+    
+    
+    
+    
+}

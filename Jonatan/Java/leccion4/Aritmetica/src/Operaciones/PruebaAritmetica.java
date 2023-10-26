8b80c75b63a636f225b8d5a45a7c6c8bb321e3d1
Jonathan Burgos <burgosjona@gmail.com>
2023-10-25 22:14:51 -0300

    Add clase 7 POO y encapsulamientos

--- Jonatan/Java/leccion4/Aritmetica/src/Operaciones/PruebaAritmetica.java
+++ Jonatan/Java/leccion4/Aritmetica/src/Operaciones/PruebaAritmetica.java
@@ -28,14 +28,40 @@ public class PruebaAritmetica {
         System.out.println("aritmetica2 = " + aritmetica2.b);
         //aritmetica1 = null;//mala practica pra liberar memoria
         //System.gc(); es un recolector de basura autentico(Es innsesario ya que al terminar de ejecutar se limpia la memoria autimaticamente)
+        Persona persona = new Persona("Ariel","Bentancud");//creamos nuevo obj String
+        System.out.println("persona = " + persona);
+        System.out.println("Persona nombnre: " + persona.nombre);
+        System.out.println("Persona apellido: " + persona.apellido);
     }
     
+    //MODULARIDAD CREAMOS UN NUEVO METODO
     public static void miMetodo(){//nuevo metodo
         //int a = 10;
         System.out.println("Aqui hay otro metodo");
         
+    }  
+}
+    //CREAMOS UNA NUEVA CLASE
+    //a esta clase solo se puede acceder si estamos en el mismo paquete
+class Persona{// al crear 23 clases en la misma plantilla no pueden haber 2 clases PUBLICAS
+    String nombre;
+    String apellido;
+    
+    Persona(String nombre, String apellido){//Constructor
+        super();//LLamada  aconstrucro de la clase Padre objet
+        //Imprimir imprimir = new Imprimir();
+        new Imprimir().imprimir(this);
+        this.apellido = apellido;
+        this.nombre = nombre;
+        System.out.println("Objeto persona usando this: "+this);
     }
-    
-    
-    
+}
+class Imprimir{//constructor
+    public Imprimir(){//en constructor de la clase padre, para reservar memoria
+        super();
+    }
+    public void imprimir(Persona persona){//metodo
+        System.out.println("Persona desde la clase Imprimir: "+persona);
+        System.out.println("Impresion del objeto actual (this): "+this);
+    }
 }

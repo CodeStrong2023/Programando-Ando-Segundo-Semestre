e6281a182c5435de98fce7d254e6525a96069237
Nicolas Sini <136397673+nicolassini@users.noreply.github.com>
2023-10-30 13:23:58 -0300

    Clase 9 JavaScript: Algunos Operadores y Algunas Funciones
    Carga manual de archivos desde GitHub

--- Nicolas/JavaScript/Leccion03/03-01-OperadoresJavaScript.js
+++ Nicolas/JavaScript/Leccion03/03-01-OperadoresJavaScript.js
@@ -25,3 +25,44 @@ if( dentroRango >= valMin && dentroRango <= valMax) {
 else {
     console.log("No esta dentro del rango")
 }
+
+//Ejercicio : Si el padre puede asistir al juego de su hijo
+let vacaciones = true, diaDescanso = true;
+if(vacaciones || diaDescanso){
+    console.log("El padre puede asistir al juego de su hijo")
+}
+else{
+    console.log("El padre No puede asistir al juego de su hijo")
+}
+
+//Operador ternario
+let resultado2 = 3 > 2 ? "Verdadero" : "Falso";
+console.log(resultado2) 
+let numero = 9;
+resultado2 = numero % 2 == 0 ? "es Par" : "es Impar";
+console.log(resultado2)
+
+// Convertir String a Number
+let miNumero = "10"; // Es una cadena
+console.log(typeof miNumero);
+let edad2 = Number(miNumero); // Esta es una funcion
+console.log(typeof edad2);
+//Funcion isNaN
+if(isNaN(edad2)){ //No es un numero = is Not a Number (devuelve un resultado booleano)
+    console.log("Esta variable no contiene solo numeros")
+}
+else{
+    
+
+
+
+    if(edad2 >= 18){
+        console.log("Puede votar");
+    }
+    else{
+        console.log("No puede votar, es menor de edad");
+    }
+}
+let resultado3 = edad2 >= 18 ? "Puede votar" : "No puede votar, es menor de edad";
+console.log(resultado3)
+

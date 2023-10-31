c8e485155eb29546008afa595c2db1f0717effdb
Ezequiel Lorenz <eze_lorenz@hotmail.com>
2023-10-31 11:28:20 -0300

    Programacion 2, Java, clase 10, Paquetes parte 1 y 2

---
+++ Ezequiel/Java/Leccion7/Paquetes/src/ar/com/codesystem/Utileria.java
@@ -0,0 +1,12 @@
+
+package ar.com.codesystem;
+
+/**
+ *
+ * @author Charly Cimino
+ * Aprendé más Java en mi canal: https://www.youtube.com/c/CharlyCimino
+ * Encontrá más código en mi repo de GitHub: https://github.com/CharlyCimino
+ */
+public class Utileria {
+
+}

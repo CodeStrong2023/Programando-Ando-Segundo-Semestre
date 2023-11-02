e520026a1380c338c0aa62d73a7e16cd650d307b
Ezequiel Lorenz <eze_lorenz@hotmail.com>
2023-11-02 10:20:09 -0300

    Laboratorio 2, Python, Clase 10, Ejercicio de tarea en clase Persona2

--- Ezequiel/Python/pythonProject/Persona2.py
+++ Ezequiel/Python/pythonProject/Persona2.py
@@ -32,10 +32,10 @@ class Persona2:
         print('Estamos utilizando el metodo get')
         return self._edad
 
-    # @nombre.setter  # decoratos
-    # def edad(self, edad):  # Setter
-    #     print('Estamos utilizando el metodo set')
-    #     self._edad = edad
+    @nombre.setter  # decoratos
+    def edad(self, edad):  # Setter
+        print('Estamos utilizando el metodo set')
+        self._edad = edad
 
 persona1 = Persona2('Ariel', 'Betancud', 41)
 print(persona1.nombre) #llamamos al metodo getter
@@ -47,3 +47,24 @@ print(persona1.nomstrar_detalles()) # Llamamos el metodo mostrar detalles
 
 #Atributo read-only seria la edad porque no tiene el metodo set
 print(persona1.edad)
+
+# Tarea crear tres objetos mas, utilizando los metodos getters and setters
+# para modificar, y mostrar los cambios
+
+persona2 = Persona2('María', 'López', 30)
+persona2.nombre = 'Laura'
+persona2.apellido = 'González'
+persona2.edad = 35
+print(persona2.nomstrar_detalles())
+
+persona3 = Persona2('Pedro', 'Gómez', 25)
+persona3.nombre = 'Roberto'
+persona3.apellido = 'Martínez'
+persona3.edad = 28
+print(persona3.nomstrar_detalles())
+
+persona4 = Persona2('Luis', 'Perez', 35)
+persona4.nombre = 'Elena'
+persona4.apellido = 'Ramírez'
+persona4.edad = 45
+print(persona4.nomstrar_detalles())

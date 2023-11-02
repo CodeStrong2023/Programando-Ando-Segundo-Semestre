9a05f84aa1997ab3e35b7f3a67ff0451d4285bdc
Ezequiel Lorenz <eze_lorenz@hotmail.com>
2023-11-02 12:08:05 -0300

    Laboratorio 2, JavaScript, clase 10, realizacion de ejercicios

--- Ezequiel/JavaScript/Leccion04/04-01-Ejercicios.js
+++ Ezequiel/JavaScript/Leccion04/04-01-Ejercicios.js
@@ -46,3 +46,82 @@ if (true) {
 console.log(edad2);
 
 /* const se utiliza para valores constantes que no pueden ser reasignados */
+
+//Ejercicio 1: Calcular estacion del anio
+let mes = 4;
+let estacion;
+
+if (mes == 1 || mes == 2 || mes == 12) {
+    estacion = "Verano";
+}
+
+else if (mes == 3 || mes == 4 || mes == 5) {
+    estacion = "Otonio";
+}
+
+else if (mes == 6 || mes == 7 || mes == 8) {
+    estacion = "Invierno";
+}
+
+else if (mes == 9 || mes == 10 || mes == 11) {
+    estacion = "Primavera";
+}
+else {
+    estacion = "Valor incorrecto"
+}
+
+console.log("El valor corresponde a la estacion de: " + estacion)
+
+//Ejercicio 2: Hora del dia
+/*
+de 6 a 11 nos saluda: Good Morning
+
+de 12 a 16 nos saluda: Good Morning
+
+de 17 a 19 nos saluda: Good Morning
+
+de 20 a 23 nos saluda: Good Morning
+
+Trabajaremos con 24hrs
+*/
+
+let horaDia = 9;
+let mensaje;
+if (horaDia >= 6 && horaDia <= 11) {
+    mensaje = "Good Morning"
+}
+else if (horaDia >= 12 && horaDia <= 16) {
+    mensaje = "Good Afternoom"
+}
+else if (horaDia >= 17 && horaDia <= 19) {
+    mensaje = "Good evening"
+}
+else if (horaDia >= 20 && horaDia <= 21) {
+    mensaje = "Good night"
+}
+else {
+    mensaje = "Valor Incorrecto"
+}
+console.log(mensaje)
+
+//Estructura switch (la sintaxis es igual a JAVA)
+switch (mes) {
+    case 1: case 2: case 12:
+        estacion = "Verano";
+        break;
+    
+    case 3: case 4: case 5:
+        estacion = "Otonio";
+        break;
+    
+    case 6: case 7: case 8:
+        estacion = "Invierno";
+        break;
+    
+    case 9: case 10: case 11:
+        estacion = "Primavera";
+        break;
+    default:
+        estacion = "Valor Incorrecto";
+}
+console.log("Bienvenido a la estacion de " + estacion)

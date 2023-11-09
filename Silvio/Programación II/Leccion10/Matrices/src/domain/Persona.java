85502508791e303259eb2738b9f4f9b73f2ffcb6
Silvio Stefanucci <silvioshredderstefanucci@gmail.com>
2023-11-09 01:59:07 -0300

    L10 Java

---
+++ Silvio/Programación II/Leccion10/Matrices/src/domain/Persona.java
@@ -0,0 +1,30 @@
+package domain;
+
+public class Persona {
+    private String nombre;
+
+    public Persona(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Persona{");
+        sb.append("nombre=").append(nombre);
+        sb.append('}');
+        sb.append(' ');
+        sb.append(super.toString());
+        return sb.toString();
+    }
+    
+    
+}

70a3e3972e4a8720c964f4846e24266e49ea7f83
Jonathan Burgos <burgosjona@gmail.com>
2023-11-10 00:31:07 -0300

    Clase 11 POO PYTHON Terminado

--- Jonatan/Python/Leccion7/Persona.py
+++ Jonatan/Python/Leccion7/Persona.py
@@ -18,6 +18,9 @@ class Persona:
     @edad.setter
     def edad(self, edad):
         self._edad = edad
+
+    def __str__(self):#override = sobreescribir
+        return f'Persona: [Nombre {self._nombre}, Edad: {self._edad} ]'
 class Empleado(Persona):#Empleado es hija de la clase persona
     def __init__(self, nombre, edad, sueldo):
         super().__init__(nombre, edad)
@@ -30,6 +33,9 @@ class Empleado(Persona):#Empleado es hija de la clase persona
     @sueldo.setter
     def sueldo(self, sueldo):
         self._sueldo = sueldo
+
+    def __str__(self):
+        return f'Empleado: [ Sueldo: {self._sueldo} ] {super().__str__()}'
 
 empleado1 = Empleado('Antonio', 43, 75000)
 print(empleado1.nombre)

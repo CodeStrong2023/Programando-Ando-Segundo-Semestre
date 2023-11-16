ee12d7831602eb6c4b6b235c7f668392786b61a7
Jonathan Burgos <burgosjona@gmail.com>
2023-11-15 21:14:30 -0300

    Agregando orden 2 y orden 3 a Ventas Test

--- Jonatan/Java/leccion11/Ventas/src/ar/com/codesystem/ventas/test/VentasTest.java
+++ Jonatan/Java/leccion11/Ventas/src/ar/com/codesystem/ventas/test/VentasTest.java
@@ -11,23 +11,38 @@ public class VentasTest {
         //agregando mas objetos de tipo productos
         Producto producto3 = new Producto("Remera", 6000.00);
         Producto producto4 = new Producto("Gorra", 3000.00);
-        Producto producto5 = new Producto("Zapatillas", 40000.00);
+        Producto producto5 = new Producto("Remera Mangas Larg", 3500.00);
+        Producto producto6 = new Producto("Musculosa", 4000.00);
+        Producto producto7 = new Producto("Vestido", 7000.00);
+        Producto producto8 = new Producto("Mameluco", 14000.00);
+        Producto producto9 = new Producto("Short de Banio", 4000.00);
+        Producto producto10 = new Producto("Reloj", 20000.00);
         
         
         Orden orden1 = new Orden();
+        Orden orden2 = new Orden();
+        Orden orden3 = new Orden();
         
         
         //Agregamos productos al arreglo
         orden1.agregarProducto(producto1);
         orden1.agregarProducto(producto2);
         orden1.agregarProducto(producto3);
-        orden1.agregarProducto(producto4);
-        orden1.agregarProducto(producto5);
+        orden2.agregarProducto(producto4);
+        orden2.agregarProducto(producto5);
+        orden2.agregarProducto(producto6);
+        orden2.agregarProducto(producto7);
+        orden3.agregarProducto(producto8);
+        orden3.agregarProducto(producto9);
+        orden3.agregarProducto(producto10);
+        
         
         
         orden1.mostrarOrden();
+        orden2.mostrarOrden();
+        orden3.mostrarOrden();
         
-        Orden orden2 = new Orden();
+        
         
         
         
